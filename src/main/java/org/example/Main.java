89da910c5eef5bf664d441d05dbc61ad7d399542
Aurora <programming.aurora@gmail.com>
2022-11-11 20:02:59 +0000

    Basic store structure.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello world!");
+
+	public static void main(String[] args) throws Exception {
+		Store store = new Store("reviews.db");
+
+		store.close();
 	}
 }
--- src/main/java/org/example/Store.java
+++ src/main/java/org/example/Store.java
@@ -1,7 +1,17 @@
 package org.example;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Store {
-	Connection connection;
+	private final Connection connection;
+
+	public Store(String path) throws SQLException {
+		connection = DriverManager.getConnection("jdbc:sqlite:" + path);
+	}
+
+	public void close() throws SQLException {
+		connection.close();
+	}
 }
